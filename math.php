005fd57e962a9689b593e6abd1dc05b8bf7b34c7
Jarod <lee.jarod@gmail.com>
2023-10-28 14:48:38 +0800

    use * / with no .

--- math.php
+++ math.php
@@ -105,10 +105,10 @@
     <h1><a href="http://s.aqde.net/zuowen/">每天口算練習100分</a></h1>
     
 
-<p>问题 1：60 - 35</p>
+<p>问题 1：81 × 3</p>
 
 <form method="post">
-    <input type="hidden" name="correctAnswer" value="25">
+    <input type="hidden" name="correctAnswer" value="243">
     <input type="hidden" name="score" value="0">
     <input type="hidden" name="questionNumber" value="1">
     <label for="answer">请输入答案：</label>
